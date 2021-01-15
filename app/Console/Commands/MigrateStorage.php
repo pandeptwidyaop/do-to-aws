c3476d6b5cd450be070f4faca2a8ebecb11b1559
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2021-01-15 17:52:13 +0800

    Done

--- app/Console/Commands/MigrateStorage.php
+++ app/Console/Commands/MigrateStorage.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Storage;
 
 class MigrateStorage extends Command
@@ -38,16 +39,21 @@ class MigrateStorage extends Command
      */
     public function handle()
     {
-        $files = Storage::disk('spaces')->allFiles();
+        if (Cache::has('do_files')) {
+            $files = json_decode(Cache::get('do_files'), true);
+        } else {
+            $files = Storage::disk('spaces')->allFiles();
+            Cache::put('do_files', json_encode($files), 600);
+        }
         $this->info("Collecting " . count($files) . " files.");
         $progress = $this->output->createProgressBar(count($files));
         $progress->start();
         foreach ($files as $key => $file) {
-            $this->info("$key. Copying $file");
-            $this->newLine();
-            Storage::disk('s3')->put($file, Storage::disk('spaces')->get($file));
+            if (!Storage::disk('s3')->exists($file)) {
+                Storage::disk('s3')->put($file, Storage::disk('spaces')->get($file));
+                sleep(1);
+            }
             $progress->advance();
-            sleep(1);
         }
         $progress->finish();
     }
