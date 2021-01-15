fe54651b17e269b5ab8ffe416a325ab5a84f0d12
Pande Putu Widya Oktapratama <widya.oktapratama@gmail.com>
2021-01-15 17:10:23 +0800

    Init

---
+++ app/Console/Commands/MigrateStorage.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
+
+class MigrateStorage extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'storage:migrate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Migrate all file from DO Spaces to AWS S3';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $files = Storage::disk('spaces')->allFiles();
+        $this->info("Collecting " . count($files) . " files.");
+        $progress = $this->output->createProgressBar(count($files));
+        $progress->start();
+        foreach ($files as $file) {
+            $this->info("Copying $file");
+            Storage::disk('s3')->put($file, Storage::disk('spaces')->get($file));
+            $progress->advance();
+            sleep(1);
+        }
+        $progress->finish();
+    }
+}
